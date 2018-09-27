4c507651be2994b334f34483b9fd07b2b9bc2276
Rajesh P. <rajesh.pethe@gmail.com>
2018-09-27 19:02:33 +0530

    Added debug, still trying to figure out. Still works as original.

--- minimumSumSlices.py
+++ minimumSumSlices.py
@@ -6,10 +6,12 @@
 # a = [int(i) for i in input().split()]
 
 
-n = 4
+# n = 4
 a = [3, 1, 2, 4]
 # a = [48,87,27]
+# a = [1, 2, 3, 4]
 
+n = len(a)
 
 b = [0] * (n+1)
 
@@ -25,12 +27,17 @@ for i in range(n):
     b[ a[i] ] = i
 
 print(b)
+print()
 
 for i in range(n, 0, -1):
     m = b[i]
     x, y = l[m], r[m]
     ans += i * (m - x + 1) * (y - m + 1)
-    print(str(i) + ' * ' + str((m - x + 1)) + ' * ' + str((y - m + 1)))
+    print('i=', i, 'm=', m, 'x=', x, 'y=', y, 'calculations = ', str(i) + ' * ' + str((m - x + 1)) + ' * ' + str((y - m + 1)))
+    print()
+    
     l[y+1], r[x-1] = x, y
-    print(i, m, x, y, ans, l, r)
+    
+    print(ans, l, r)
+    print()
 print(ans)
